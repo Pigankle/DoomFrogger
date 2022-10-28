5e04d321e96de5975f7db1b8e79de119531d9e14
Pigankle <Pigankle@gmail.com>
2022-10-28 13:05:22 -0700

    * renamed car_manager.py --> car_factory.py

---
+++ car_factory.py
@@ -0,0 +1,31 @@
+"""
+Car Factory...> Makes Cars and puts them where they belong.
+"""
+from random import choice, randint
+from carbinger import Carbinger
+from constants import *
+
+class CarFactory:
+    def new_car():
+        """create a new random carbinger at a random side of the board"""
+        ind = randint(0, len(CAR_TYPES) - 1)
+        new_car = Carbinger()
+        new_car.threat = CAR_TYPES[ind].threat
+        new_car.color = CAR_TYPES[ind].color
+        new_car.speed  = -CAR_TYPES[ind].speed
+
+        leftside = randint(0,1)
+        new_x = SCREEN_WIDTH
+        new_angle = 0
+        new_y = randint(25,SCREEN_HEIGHT - 25)
+        texture1 = arcade.load_texture(RESOURCE_DIR / CAR_DEFAULT_ICON)
+        if leftside:
+            new_x = 0
+            new_car.speed = CAR_TYPES[ind].speed
+            texture1 = arcade.load_texture(RESOURCE_DIR /CAR_DEFAULT_ICON, mirrored = True)
+        new_car.texture = texture1
+        new_car.center_x = new_x
+        new_car.center_y= new_y
+        new_car.angle= new_angle
+        new_car.size = CAR_SCALE
+        return new_car
