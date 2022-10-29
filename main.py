3e1127e857261f1e7ae91ab4ed8bfcb542c619b4
Pigankle <Pigankle@gmail.com>
2022-10-28 20:26:17 -0700

    cleanup and docstring

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import arcade
-
 from constants import *
 from game_view import GameView
 #from instruction_view import InstructionView
 
 
 def main():
+"""launch a new game window with start_view"""
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     start_view = GameView()
     start_view.setup()
